73075199b50deaddc6e70b2125f3744630e55159
Patrick Dooley <patrickthe8@live.com>
2020-11-02 15:12:40 -0500

    Created classes for accounts and ATM cards.

---
+++ src/ATMMachine.py
@@ -0,0 +1,11 @@
+"""
+Author: Team 2
+Description: Class for ATM machines.
+"""
+
+
+# todo: Create class for ATM Machines
+class ATMMachine:
+
+    def __init__(self):
+        self.data = []
--- src/User.py
+++ src/User.py
@@ -1,1 +1,29 @@
-print("Hello, world!")
+"""
+Author: Team 2
+Description: Contains user functions for ATM machines.
+Sub menus: Cash transfer, cash withdrawal, balance enquiry.
+"""
+
+
+# Allows the user to transfer money into a different account.
+def cash_transfer():
+    # todo: allow transfer of money to other account
+    return
+
+
+# Allows the user to withdraw money from their account.
+def cash_withdrawal():
+    # todo: allow withdrawal of money from user's account
+    return
+
+
+# Allows the user to check their balance.
+def balance_enquiry():
+    # todo: allow user to check balance
+    return
+
+
+# Provides a menu for the user.
+def main():
+    # todo: USER MODULE
+    return
